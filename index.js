436bc49c4b93c37d53de00cab38a8440dc88b617
wei.liu <wei.liu@vechain.com>
2019-07-10 15:42:58 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const Koa = require('koa')
+const fs = require('fs')
 const app = new Koa()
 
-app.use( async ( ctx ) => {
-  ctx.body = 'hello koa2 test'
+app.use(async(ctx,next) => {
+        console.log("ctx.method",ctx.method);
+        console.log("ctx.path:",ctx.path);
+        next();
+});
+
+// 装载所有子路由
+const Router = require('koa-router')
+let router = new Router()
+router.get('/api', async ( ctx )=>{
+  console.log("API");
+  let html = `
+    <ul>
+      <li><a href="/api/helloworld">/page/helloworld</a></li>
+      <li><a href="/api/404">/page/404</a></li>
+    </ul>
+  `
+  ctx.body = html
+})
+router.get('/api/404', async ( ctx )=>{
+  ctx.body = '404 page!'
+})
+router.get('/api/helloworld', async ( ctx )=>{
+  ctx.body = 'helloworld page!'
 })
 
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(3000, () => {
+  console.log('[demo] route-use-middleware is starting at port 3000')
+})
